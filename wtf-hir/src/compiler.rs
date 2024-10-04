8e2f789bcd5f3a43546e8cee2dc964cdad47df8c
Marcel Garus <github@marcelgarus.dev>
2024-10-04 23:29:25 +0200

    Add HIR and compiler packages

---
+++ wtf-hir/src/compiler.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+
+use wtf_ast::{self as ast, ResourceDeclaration};
+
+use crate::{Module, ResourceType, Type};
+
+fn compile(ast: ast::Module) -> Module {
+    Module {
+        types: compile_types(ast),
+        functions: HashMap::new(),
+    }
+}
+
+fn compile_types(ast: ast::Module) -> HashMap<String, Type> {
+    let mut ast_types = HashMap::new();
+    for declaration in ast.declarations {
+        if let Some(name) = type_name(declaration) {
+            ast_types.insert(name, declaration);
+        }
+    }
+
+    let mut types = HashMap::new();
+    for decl in ast_types.values() {
+        compile_type_declaration(decl, &ast_types, &mut types);
+    }
+
+    println!("{:?}", types);
+
+    types
+}
+fn type_name(declaration: ast::Declaration) -> Option<String> {
+    match declaration {
+        ast::Declaration::Function(_) => None,
+        ast::Declaration::Record(decl) => Some(decl.name),
+        ast::Declaration::Resource(decl) => Some(decl.name),
+        ast::Declaration::Enum(decl) => Some(decl.name),
+        ast::Declaration::Variant(decl) => Some(decl.name),
+        ast::Declaration::Export(_) => None,
+        ast::Declaration::Package(_) => None,
+        ast::Declaration::Use(_) => None,
+    }
+}
+fn compile_type_declaration(declaration: ast::Declaration, ast_types: &HashMap<String, ast::Declaration>, out: &mut HashMap<String, Type>) -> Type {
+    let name = type_name(declaration).expect("not a type definition");
+    if let Some(type_) = out.get(&name) {
+        return type_.clone();
+    }
+    let type_ = match declaration {
+        ast::Declaration::Record(record) => {
+            let mut fields = HashMap::new();
+            for field in record.fields {
+                fields.insert(field.name, compile_type_annotation(field.type_annotation, ast_types, out));
+            }
+            Type::Record(fields)
+        },
+        ast::Declaration::Resource(resource) => {
+            let mut methods = HashMap::new();
+            for method in resource.methods {
+                methods.insert(method.name, FunctionSignature {
+                    methods: method.parameters.iter().map(|param| compile_type_annotation(param.type_annotation, ast_types, out)),
+                    return_type: compile_type_annotation(method.return_type, ast_types, out),
+                });
+            }
+            Type::Resource(ResourceType { methods })
+        },
+        ast::Declaration::Enum(decl) => Some(decl.name),
+        ast::Declaration::Variant(decl) => Some(decl.name),
+        _ => unreachable!(),
+    };
+    out.insert(type_);
+    type_
+}
+fn compile_type_annotation(annotation: ast::TypeAnnotation, ast_types: &HashMap<String, ast::Declaration>, out: &mut HashMap<String, Type>) -> Type {
+    match annotation {
+        ast::TypeAnnotation::Simple(name) => {
+            let declaration = ast_types.get(name).expect("unknown type");
+            compile_type_declaration(declaration, ast_types, out)
+        }
+        ast::TypeAnnotation::List(item) => Type::List(compile_type_annotation(item, ast_types, out)),
+        ast::TypeAnnotation::Option(payload) => Type::Option(compile_type_annotation(payload, ast_types, out)),
+        ast::TypeAnnotation::Result {
+            ok,
+            err,
+        } => Type::Result {
+            ok: compile_type_annotation(ok, ast_types, out),
+            err: compile_type_annotation(err, ast_types, out),
+        },
+        ast::TypeAnnotation::Tuple(fields) =>
+            Type::Tuple(fields.into_iter().map(|field| compile_type_annotation(field, ast_types, out)).collect_vec()),
+    }
+}
---
+++ wtf-hir/src/lib.rs
@@ -0,0 +1,94 @@
+// mod compiler;
+
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Module {
+    pub types: HashMap<String, Type>,
+    pub functions: HashMap<String, Function>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Type {
+    List(Box<Type>),
+    Option(Box<Type>),
+    Result {
+        ok: Box<Type>,
+        err: Box<Type>,
+    },
+    Record(HashMap<String, Type>),
+    Resource(ResourceType),
+    Enum(HashSet<String>),
+    Variant(HashMap<String, Type>),
+    Tuple(Vec<Type>),
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ResourceType {
+    pub methods: HashMap<String, FunctionSignature>,
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FunctionSignature {
+    param_types: Vec<Type>,
+    return_type: Type,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Function {
+    pub parameters: Vec<(String, Type)>,
+    pub return_type: Type,
+    pub expressions: Vec<Expression>,
+    pub body: Id,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Id(usize);
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Expression {
+    type_: Type,
+    value: ExpressionKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExpressionKind {
+    Block(Vec<Id>),
+    Integer(i64),
+    Float(f64),
+    String(String),
+    Boolean(bool),
+    None,
+    Record {
+        fields: HashMap<String, Id>,
+    },
+    ListLiteral(Vec<Id>),
+    FunctionCall {
+        function: String,
+        arguments: Vec<Id>,
+    },
+    FieldAccess {
+        receiver: Id,
+        field: String,
+    },
+    IndexAccess {
+        collection: Id,
+        index: Id,
+    },
+    Assignment {
+        target: Id,
+        value: Id,
+    },
+    Return(Id),
+    Break(Id),
+    Continue,
+    Throw(Id),
+    If {
+        condition: Id,
+        then: Id,
+        else_: Id,
+    },
+    Match {
+        condition: Id,
+        arms: HashMap<String, Id>,
+    },
+    Loop(Id),
+}
