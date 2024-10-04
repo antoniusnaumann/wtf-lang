d275a6ab6dd171847f8e6015778f0b8bc3f980ea
Antonius Naumann <git@antonius.dev>
2024-10-04 23:40:35 +0200

    Use workspace edition in hir crate manifest

--- wtf-hir/src/compiler.rs
+++ wtf-hir/src/compiler.rs
@@ -40,7 +40,11 @@ fn type_name(declaration: ast::Declaration) -> Option<String> {
         ast::Declaration::Use(_) => None,
     }
 }
-fn compile_type_declaration(declaration: ast::Declaration, ast_types: &HashMap<String, ast::Declaration>, out: &mut HashMap<String, Type>) -> Type {
+fn compile_type_declaration(
+    declaration: ast::Declaration,
+    ast_types: &HashMap<String, ast::Declaration>,
+    out: &mut HashMap<String, Type>,
+) -> Type {
     let name = type_name(declaration).expect("not a type definition");
     if let Some(type_) = out.get(&name) {
         return type_.clone();
@@ -49,20 +53,28 @@ fn compile_type_declaration(declaration: ast::Declaration, ast_types: &HashMap<S
         ast::Declaration::Record(record) => {
             let mut fields = HashMap::new();
             for field in record.fields {
-                fields.insert(field.name, compile_type_annotation(field.type_annotation, ast_types, out));
+                fields.insert(
+                    field.name,
+                    compile_type_annotation(field.type_annotation, ast_types, out),
+                );
             }
             Type::Record(fields)
-        },
+        }
         ast::Declaration::Resource(resource) => {
             let mut methods = HashMap::new();
             for method in resource.methods {
-                methods.insert(method.name, FunctionSignature {
-                    methods: method.parameters.iter().map(|param| compile_type_annotation(param.type_annotation, ast_types, out)),
-                    return_type: compile_type_annotation(method.return_type, ast_types, out),
-                });
+                methods.insert(
+                    method.name,
+                    FunctionSignature {
+                        methods: method.parameters.iter().map(|param| {
+                            compile_type_annotation(param.type_annotation, ast_types, out)
+                        }),
+                        return_type: compile_type_annotation(method.return_type, ast_types, out),
+                    },
+                );
             }
             Type::Resource(ResourceType { methods })
-        },
+        }
         ast::Declaration::Enum(decl) => Some(decl.name),
         ast::Declaration::Variant(decl) => Some(decl.name),
         _ => unreachable!(),
@@ -70,22 +82,31 @@ fn compile_type_declaration(declaration: ast::Declaration, ast_types: &HashMap<S
     out.insert(type_);
     type_
 }
-fn compile_type_annotation(annotation: ast::TypeAnnotation, ast_types: &HashMap<String, ast::Declaration>, out: &mut HashMap<String, Type>) -> Type {
+fn compile_type_annotation(
+    annotation: ast::TypeAnnotation,
+    ast_types: &HashMap<String, ast::Declaration>,
+    out: &mut HashMap<String, Type>,
+) -> Type {
     match annotation {
         ast::TypeAnnotation::Simple(name) => {
             let declaration = ast_types.get(name).expect("unknown type");
             compile_type_declaration(declaration, ast_types, out)
         }
-        ast::TypeAnnotation::List(item) => Type::List(compile_type_annotation(item, ast_types, out)),
-        ast::TypeAnnotation::Option(payload) => Type::Option(compile_type_annotation(payload, ast_types, out)),
-        ast::TypeAnnotation::Result {
-            ok,
-            err,
-        } => Type::Result {
+        ast::TypeAnnotation::List(item) => {
+            Type::List(compile_type_annotation(item, ast_types, out))
+        }
+        ast::TypeAnnotation::Option(payload) => {
+            Type::Option(compile_type_annotation(payload, ast_types, out))
+        }
+        ast::TypeAnnotation::Result { ok, err } => Type::Result {
             ok: compile_type_annotation(ok, ast_types, out),
             err: compile_type_annotation(err, ast_types, out),
         },
-        ast::TypeAnnotation::Tuple(fields) =>
-            Type::Tuple(fields.into_iter().map(|field| compile_type_annotation(field, ast_types, out)).collect_vec()),
+        ast::TypeAnnotation::Tuple(fields) => Type::Tuple(
+            fields
+                .into_iter()
+                .map(|field| compile_type_annotation(field, ast_types, out))
+                .collect_vec(),
+        ),
     }
 }
