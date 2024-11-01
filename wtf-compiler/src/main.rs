6fbc4b31b6b4dd1b5e661d88977e8ab5a3387a1f
Antonius Naumann <git@antonius.dev>
2024-11-01 22:22:19 +0100

    Supply compiler path as command line argument

--- wtf-compiler/src/main.rs
+++ wtf-compiler/src/main.rs
@@ -1,33 +1,16 @@
-use std::fs;
+use std::{env, fs};
 
 use wtf_parser::parser::Parser;
 
 fn main() {
     println!("WTF!");
 
-    let input = r#"
-        package wtf:example@0.0.1
-    
-        func double_cap(x: s64, cap: s64) -> s64 {
-            let y = x * 2
-            if y > cap {
-               return cap
-            } else {
-               return y
-            }
-        }
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
 
-        record point {
-            x: s64 
-            y: s64
-        }
+    let input = fs::read_to_string(file_path).unwrap();
 
-        func double_point(p: point) -> point {
-            return p
-        }
-        "#;
-
-    let mut parser = Parser::new(input);
+    let mut parser = Parser::new(&input);
     println!();
     println!("===== AST =====");
     let ast = parser.parse_module().expect("No AST.");
